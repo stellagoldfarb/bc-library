94b039cdf9b3dfb63f879a824456d3d8b0fbcdf8
Marie Stella Goldfarb <stella@Maries-MacBook-Air.local>
2022-01-31 16:59:14 -0500

    library

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const {initializeApp, cert} = require ("firebase-admin/app");
+const {getFirestore} = require ("firebase-admin/firestore");
+
+const credentials = require("../credentials.json");
+
+initializeApp({
+    credential: cert(credentials),
+})
+
+const db = getFirestore();
+
+const userCollection = db.collection("users");
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (request, response) => {
+    const userCollection = db.collection("users");
+
+    userCollection
+    .get()
+    .then(snapshot => {
+
+        const users = []
+        snapshot.forEach (doc => {
+            users.push({id: doc.id, ...doc.data()})
+        })
+
+        response.send(users)
+    })
+
+    
     response.send('Hello World!');
 
 })
